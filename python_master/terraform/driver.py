1941e70197df8b473e78204146e77effb6a21b72
CodeGeta <akshaykatira294@gmail.com>
2021-03-22 15:39:46 -0400

    Python code for terraform

---
+++ python_master/terraform/driver.py
@@ -0,0 +1,15 @@
+
+from terraform import *
+
+
+init()
+run()
+# destroy()
+
+
+# Driver.py interacts with test1.py
+
+
+
+# test1 will interact with [terraform] [ansible] SSH.py Scenario - 1
+# test2 will interact with terraform.py ansible.py SSH.py Scenario - 2
