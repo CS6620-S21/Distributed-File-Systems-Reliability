22feb12f9134643f78151e3876d612ad18086be9
CodeGeta <akshaykatira294@gmail.com>
2021-03-25 13:14:38 -0400

    Standardised ip format

--- python_master/terraform/driver.py
+++ python_master/terraform/driver.py
@@ -22,22 +22,39 @@ def destroyInfrastructure():
     resetState()
 
 def getIPs():
-    output()
+    # output()
     a_file = open("./output.json", "r")
     json_object = json.load(a_file)
     a_file.close()
-    return json_object
 
+    hosts_inventory_dict = {'master': {},'metalogger': {},'chunkserver': {},'client': {}}
+
+    for val in  json_object.keys():
+
+        if("chunkserver" in val):
+            hosts_inventory_dict["chunkserver"][val] = json_object[val]["value"]
+
+        if("metalogger" in val):
+            hosts_inventory_dict["metalogger"][val] = json_object[val]["value"]
+
+        if("client" in val):
+            hosts_inventory_dict["client"][val] = json_object[val]["value"]
+
+        if("master" in val):
+            hosts_inventory_dict["master"][val] = json_object[val]["value"]
+
+    return hosts_inventory_dict
 
 
 def deleteChunkServer():
     print("Client Destroyed")
 
+
+
 # addChunkServer()
-
-removeChunkServer()
-
+# removeChunkServer()
 # Users/aksha/OneDrive/Documents/projects/git/Distributed-File-Systems-Reliability/python_master/terraform
 # destroyInfrastructure()
-# createInfrastructure(1, 2, 1, 2)
+# createInfrastructure(1, 0, 1, 1)
+# print(getIPs())
 # getIPs()
