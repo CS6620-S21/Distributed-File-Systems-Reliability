d1c61724e2a876637799e03c657868f4c1034e53
CodeGeta <akshaykatira294@gmail.com>
2021-03-26 13:01:01 -0400

    Full demo

--- python_master/main_driver.py
+++ python_master/main_driver.py
@@ -11,35 +11,35 @@ import json
 import time
 
 # Opening Input config JSON file
-# with open('input_config.json') as json_file:
-#     data = json.load(json_file)
-#
-# num_masterservers = data['mfs_num_master_servers']
-# num_metaloggers = data['mfs_num_metalogger_servers']
-# num_chunkservers = data['mfs_num_chunk_servers']
-# num_clientservers = data['mfs_num_client_servers']
+with open('input_config.json') as json_file:
+    data = json.load(json_file)
+
+num_masterservers = data['mfs_num_master_servers']
+num_metaloggers = data['mfs_num_metalogger_servers']
+num_chunkservers = data['mfs_num_chunk_servers']
+num_clientservers = data['mfs_num_client_servers']
 
 
 # Terraform
-# createInfrastructure(num_masterservers, num_chunkservers,
-#                      num_metaloggers, num_clientservers)
+createInfrastructure(num_masterservers, num_chunkservers,
+                     num_metaloggers, num_clientservers)
 hosts_inventory_dict = getIPs()
-# print(hosts_inventory_dict)
-#
-# time.sleep(120)
+print(hosts_inventory_dict)
+
+time.sleep(120)
 
 
 # Performs setup of configuration of the different vms that has been created by terraform.
 # It creates a dynamic inventory hosts file to be used by ansible, which contains the server types
 # and IP address details.
 # It then executes the different ansible playbooks for Moose FS setup across different group of servers.
-# ansible_conf = MFSAnsibleSetupVMs(data['ansible_basepath'])
-# ansible_conf.create_inventory(hosts_inventory_dict)
-# ansible_conf.execute_ansible_playbook()
+ansible_conf = MFSAnsibleSetupVMs(data['ansible_basepath'])
+ansible_conf.create_inventory(hosts_inventory_dict)
+ansible_conf.execute_ansible_playbook()
 
 
 # SSH_SCP
-# copyFile(hosts_inventory_dict)
+copyFile(hosts_inventory_dict)
 check(hosts_inventory_dict)
 
 
