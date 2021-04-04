402fd7cf88e51e7459e07c74ede59d4826eb4cac
CodeGeta <akshaykatira294@gmail.com>
2021-04-04 17:13:54 -0400

    check

--- python_master/terraform/stateModifier.py
+++ python_master/terraform/stateModifier.py
@@ -211,8 +211,8 @@ def addChunkServer():
             {
                 volumeAttachInstanceID : [
                     {
-                        "instance_id": "${openstack_compute_instance_v2" + chunkServerInstanceID +"}",
-                        "volume_id": "${openstack_blockstorage_volume_v2" + volumeInstanceID +"}"
+                        "instance_id": "${openstack_compute_instance_v2." + chunkServerInstanceID + ".id}",
+                        "volume_id": "${openstack_blockstorage_volume_v2." + volumeInstanceID + ".id}"
                     }
                 ]
             }
