d7414b84103d8f07c8413ed288141875c14d4d95
CodeGeta <akshaykatira294@gmail.com>
2021-04-07 07:39:19 -0400

    fix

--- python_master/terraform/stateModifier.py
+++ python_master/terraform/stateModifier.py
@@ -244,10 +244,10 @@ def removeResource(resourceid):
 
     json_object = fetchCurrentState()
 
-    print()
+    print("LOGS")
     print(json_object)
     print()
-    
+
     n = len(json_object["resource"])
 
     for i in range(0, n):
@@ -269,7 +269,7 @@ def removeResource(resourceid):
         json_object["output"].pop(output[i])
 
 
-    print()
+    print("LOGS")
     print(json_object)
     print()
 
