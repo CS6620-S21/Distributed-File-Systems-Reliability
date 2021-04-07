153f1b262860aa07cb14d1ce569d3bf91805f59b
CodeGeta <akshaykatira294@gmail.com>
2021-04-07 08:37:30 -0400

    checking for the chunk server

--- python_master/main_driver.py
+++ python_master/main_driver.py
@@ -44,7 +44,7 @@ createInfrastructure(num_masterservers, num_chunkservers,num_metaloggers, num_cl
 hosts_inventory_dict = getIPs()
 
 
-for key in hosts_inventory_dict['client'].keys():
+for key in hosts_inventory_dict['chunkserver'].keys():
     print("LOG")
     print(key)
     deleteResource(key)
