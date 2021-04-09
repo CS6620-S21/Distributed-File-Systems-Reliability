ba0ed2fb81432bdc06c9b152a982e7af35e617eb
Max <s@gmail.com>
2021-04-09 13:37:17 -0400

    commit for scenario4.py againn4

--- python_master/scenario4_driver.py
+++ python_master/scenario4_driver.py
@@ -4,7 +4,7 @@ from paramiko.client import AutoAddPolicy, SSHClient
 import paramiko
 import sys
 
-ab = AbstractScenarioDriver()
+ab = AbstractScenarioDriver("abc")
 run_result1 = ab.script_copy_execute_remote_vm("/home/admin_user/Distributed-File-Systems-Reliability/python_master/scripts/script4_1.sh","/home/admin_user/script4_1.sh","10.0.0.250","admin_user")
 # class Scenario4Driver(AbstractScenarioDriver):
 #
