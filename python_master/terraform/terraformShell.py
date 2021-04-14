7dfeb40785a050e36cca8e8258a2af07eb7ca61e
CodeGeta <akshaykatira294@gmail.com>
2021-04-14 17:07:06 -0400

    trying to change the working directory

--- python_master/terraform/terraformShell.py
+++ python_master/terraform/terraformShell.py
@@ -6,15 +6,15 @@ import os
 import subprocess
 
 def init():
-    subprocess.call(['sh', './terraform/terraform.starter.sh'])
+    subprocess.call(['sh', './terraform/terraform.starter.sh'], cwd="/terrafrom")
 
 def run():
-    subprocess.call(['sh', './terraform/terraform.runner1.sh'])
+    subprocess.call(['sh', './terraform/terraform.runner1.sh'], cwd="/terrafrom")
 
 
 def destroy():
-    subprocess.call(['sh', './terraform/terraform.destroy1.sh'])
+    subprocess.call(['sh', './terraform/terraform.destroy1.sh'], cwd="/terrafrom")
 
 def output():
-    subprocess.call(['sh', './terraform/terraform.output.sh'])
+    subprocess.call(['sh', './terraform/terraform.output.sh'], cwd="/terrafrom")
 
