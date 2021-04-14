1987314b503ae3c88a332585878624912a40b06e
sameersinha1606 <sameersinha1606@gmail.com>
2021-04-14 17:56:06 -0400

    adding terraform basepath

--- python_master/terraform/terraformShell.py
+++ python_master/terraform/terraformShell.py
@@ -5,16 +5,24 @@
 import os
 import subprocess
 
+terraform_basepath = "/home/admin_user/Distributed-File-Systems-Reliability/python_master/terraform"
+
+
 def init():
-    subprocess.call(['sh', './terraform/terraform.starter.sh'])
+    subprocess.call(['sh', './terraform/terraform.starter.sh'],
+                    cwd=terraform_basepath)
+
 
 def run():
-    subprocess.call(['sh', './terraform/terraform.runner1.sh'])
+    subprocess.call(['sh', './terraform/terraform.runner1.sh'],
+                    cwd=terraform_basepath)
 
 
 def destroy():
-    subprocess.call(['sh', './terraform/terraform.destroy1.sh'])
+    subprocess.call(['sh', './terraform/terraform.destroy1.sh'],
+                    cwd=terraform_basepath)
+
 
 def output():
-    subprocess.call(['sh', './terraform/terraform.output.sh'])
-
+    subprocess.call(['sh', './terraform/terraform.output.sh'],
+                    cwd=terraform_basepath)
